723233c746af5d71c39fc6ce23361615616370b0
Wessel Smit <wrsmit00@gmail.com>
2021-07-06 23:40:07 +0200

    add empty 404, detail page and css modules

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+import styles from '../styles/404.module.css'
+
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>
+}
---
+++ pages/pokemon/[id].js
@@ -0,0 +1,8 @@
+import styles from '../../styles/pokemon.module.css'
+
+
+export default function Pokemon({ data }) {
+  return (
+    <h1>Pokemon detail</h1>
+  )
+}
