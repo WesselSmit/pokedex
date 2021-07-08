d506eeeef010265c4cb4e27718b6a153eccda08d
Wessel Smit <wrsmit00@gmail.com>
2021-07-09 01:44:39 +0200

    fetch individual pokemon data

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,8 +1,34 @@
 import styles from '../../styles/pokemon.module.css'
 
 
-export default function Pokemon({ data }) {
+export default function Pokemon({ pokemon }) {
   return (
-    <h1>Pokemon detail</h1>
+    <>
+      <h1>{pokemon.name}</h1>
+    </>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  let data
+
+  try {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + context.params.id)
+    data = await res.json()
+  } catch (err) {
+    console.log('Could not fetch pokemon data.')
+  }
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      pokemon: data
+    }, 
+  }
 }
