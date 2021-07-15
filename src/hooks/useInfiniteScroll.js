ced114049b6deab79a9eaf9bee56fc7c672b8468
Wessel Smit <wrsmit00@gmail.com>
2021-07-16 01:42:55 +0200

    created custom useInfiniteScroll hook

---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+
+/*
+  Arguments explanation:
+  triggerEl - DOM node to watch using the IntersectionObserver
+  callback - function to call when 'triggerEl' is visible in the viewport
+  options (optional) - custom IntersectionObserver options to overwrite the default options with
+*/
+
+export default function useInfiniteScroll(triggerEl, callback, options) {
+  // overwrite default IntersectionObserver options if necessary
+  const config = {
+    root: options?.root || null,
+    rootMargin: options?.rootMargin || '0px 0px 0px 0px',
+    threshold: options?.threshold || 1,
+  }
+  
+  // create and initialize IntersectionObserver
+  const [observer, setObserver] = useState(null)
+
+  useEffect(() => {
+    setObserver(new IntersectionObserver(handleObserver, config))
+  }, [])
+
+  useEffect(() => {
+    if (triggerEl) {
+      observer.observe(triggerEl)
+    }
+  }, [triggerEl])
+
+
+  // save last scroll position
+  let prevYPos = 0
+
+  const handleObserver = ([entry], observer) => {
+    const yPos = entry.boundingClientRect.y
+
+    // prevent IntersectionObserver from firing on page load and firing twice on scroll
+    if (prevYPos > yPos) {
+      callback()
+    }
+
+    prevYPos = yPos
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
+import useInfiniteScroll from '../hooks/useInfiniteScroll'
 import Search from '../components/Search'
 import Cards from '../components/Cards'
 import Head from 'next/head'
@@ -7,64 +8,28 @@ import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home({ data }) {
-  // TODO werkt 'next' dit ook met useState of useRef ipv een let?
-  // let { next: nextFetchLink } = data
-  // const [next, setNext] = useState(data.next)
-  let next = data.next
+  let { next } = data
 
   const [pokemons, setPokemons] = useState(data.results)
   // const [filteredPokemons, setFilteredPokemons] = useState(pokemons)
-  
-  // TODO werkt dit ook met een useRef ipv een useState?
-  // const target = useRef(null)
+
   const [targetElement, setTargetElement] = useState(null)
-  const prevY = useRef(0) // storing the last intersection y position
-
-  // TODO: spelen met de options (rootMargin eerder zetten)
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 1.0,
-  }
-
-  // TODO: werkt de infinite scroll ook zonder de check voor 'prevY'?
-  const handleObserver = (entities, observer) => {
-    const y = entities[0].boundingClientRect.y
-
-    if (prevY.current > y) {
-      fetchImages()
-    }
-
-    prevY.current = y
-  }
-
-  // TODO (lelijke oplossing): IntersectionObserver werkt alleen met useEffect, maar useEffect kan geen waarde returnen dus moet er gebruik gemaakt worden van setState om de observer op te slaan
-  const [observer, setObserver] = useState(null)
-
-  useEffect(() => {
-    setObserver(new IntersectionObserver(handleObserver, options))
-  }, [])
-
-  // TODO werkt dit ook met useRef ipv useState? (als dit werkt dan kan de [targetElement] een lege dependency-array/[] worden)
-  useEffect(() => {
-    if (targetElement) {
-      observer.observe(targetElement)
-    }
-  }, [targetElement])
 
   const fetchImages = async () => {
-    // TODO gebruikt try catch + loading state
-    const res = await fetch(next)
-    const data = await res.json()
-
-    next = data.next
-
-    // TODO waarom werkt het een wel en het ander niet? --> VRAAG AAN SJORS
-    // setPokemons([...pokemons, ...data.results]) // werkt niet
-    setPokemons((pokemons) => [...pokemons, ...data.results]) // werkt wel
+    // TODO: check of er nog genoeg pokemons zijn om op te uit de API (als alle pokemons opgehaald zijn moet er een early return zijn)
+    try {
+      const res = await fetch(next)
+      const data = await res.json()
+  
+      next = data.next
+  
+      setPokemons((pokemons) => [...pokemons, ...data.results])
+    } catch (err) {
+      console.log('Could not fetch new pokémons: ', err)
+    }
   }
 
-  // TODO clean up de code
+  useInfiniteScroll(targetElement, fetchImages, { rootMargin: '1000px' })
   
   return (
     <>
