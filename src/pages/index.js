b403ee1bb8e85030063425075da938a55f49e443
Wessel Smit <wrsmit00@gmail.com>
2021-08-05 01:34:48 +0200

    fetch japanese pokemon names + fetch pokemon bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,11 +26,24 @@ export default function Home({ data, next }) {
   
       nextFetchLink = next
 
-      // fetch detailed data for each pokemon
+      // fetch detailed data for each pokemon (2 endpoints required: '/pokemon/:id' and '/pokemon-species/:id')
+      // the '/pokemon-species/:id' url is included in the '/pokemon/:id' data
       const fetchedPokemons = await Promise.all(
         results.map(async result => {
-          const res = await fetch(result.url)
-          return await res.json()
+          const pokemonRes = await fetch(result.url)
+          const pokemonData = await pokemonRes.json()
+          const pokemonSpeciesRes = await fetch(pokemonData.species.url)
+          const pokemonSpeciesData = await pokemonSpeciesRes.json()
+
+          // both pokemonData and pokemonSpeciesData contain an id
+          // all id's in pokemonData are unique, even 'variant' pokemons have an unique id (10000+)
+          // the pokemonSpeciesData id's are always the id's of the 'original' pokemon, even if the pokemon is a 'variant' (0 - 10000)
+          // the is always needs to be unique due to it being used to filter out pokemon 'variants'
+
+          // get rid of 'id' in pokemonSpeciesData
+          delete pokemonSpeciesData.id
+
+          return {...pokemonData, ...pokemonSpeciesData}
         })
       )
 
@@ -49,6 +62,8 @@ export default function Home({ data, next }) {
   
   // initialize infinite scroll 
   useInfiniteScroll(targetElement, fetchPokemons, { rootMargin: '1000px' })
+
+  // TODO refactor de dubbele fetch logica in een losse function (meer DRY)
 
   // TODO add loading state
   // TODO add 'load more' button for when the page isn't loading (this is necessary because if you go back to the overview page from a detail page, then you might be at the bottom of the page but since you didn't scroll the IntersectionObserver did not get triggered)
@@ -85,11 +100,24 @@ export async function getStaticProps() {
   const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40')
   const { results, next } = await res.json()
 
-  // fetch detailed data for each pokemon
+  // fetch detailed data for each pokemon (2 endpoints required: '/pokemon/:id' and '/pokemon-species/:id')
+  // the '/pokemon-species/:id' url is included in the '/pokemon/:id' data
   const pokemons = await Promise.all(
     results.map(async result => {
-      const res = await fetch(result.url)
-      return await res.json()
+      const pokemonRes = await fetch(result.url)
+      const pokemonData = await pokemonRes.json()
+      const pokemonSpeciesRes = await fetch(pokemonData.species.url)
+      const pokemonSpeciesData = await pokemonSpeciesRes.json()
+
+      // both pokemonData and pokemonSpeciesData contain an id
+      // all id's in pokemonData are unique, even 'variant' pokemons have an unique id (10000+)
+      // the pokemonSpeciesData id's are always the id's of the 'original' pokemon, even if the pokemon is a 'variant' (0 - 10000)
+      // the is always needs to be unique due to it being used to filter out pokemon 'variants'
+
+      // get rid of 'id' in pokemonSpeciesData
+      delete pokemonSpeciesData.id
+
+      return {...pokemonData, ...pokemonSpeciesData}
     })
   )
 
@@ -98,6 +126,7 @@ export async function getStaticProps() {
       data: pokemons,
       next
     },
-    revalidate: 86400 // regenerate static page every 24 hours (60 seconds x 60 minutes x 24 hours = 86400 or 1 day)
+    // regenerate static page every 24 hours (60 seconds x 60 minutes x 24 hours = 86400 seconds)
+    revalidate: 86400 
   }
 }
