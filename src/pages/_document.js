467330a83102f5bae223d4ebd3634c30ddc89148
Wessel Smit <wrsmit00@gmail.com>
2021-08-08 00:29:49 +0200

    fix google-font <link> tag in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;800&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
         </Head>
