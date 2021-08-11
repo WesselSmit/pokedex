610cfbdbc93a5059d53f63800c39995a72dc1db0
Wessel Smit <wrsmit00@gmail.com>
2021-08-11 23:51:41 +0200

    search component styling done

--- src/components/search.js
+++ src/components/search.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import SearchIcon from '../assets/searchIcon'
 import styles from '../styles/components/search.module.css'
 
+// TODO search function implementeren
 
 export default function Search() {
   const [inputElement, setInputElement] = useState(null)
@@ -9,18 +10,29 @@ export default function Search() {
   useEffect(() => {
     if (!inputElement) return
 
-    inputElement.focus()
+    focus()
   }, [inputElement])
+
+  function focus() {
+    inputElement.focus()
+  }
+
+  function reset() {
+    inputElement.value = ''
+    focus()
+  }
 
   return (
     <section className={styles.outer}>
       <div className={styles.inner}>
         <div className={styles.searchBar}>
-          <SearchIcon className={styles.searchIcon} />
-          <input type="text" placeholder="Search by name or id..." autoComplete="off" spellCheck="false" ref={setInputElement} className={styles.input} />
-        </div>
+          <label htmlFor="search" onClick={focus} className={styles.label}>
+            <SearchIcon className={styles.icon} />
+          </label>
 
-        <button className={styles.reset}>Reset</button>
+          <input type="text" id="search" placeholder="Search by name or id" maxLength="15" autoComplete="off" spellCheck="false" ref={setInputElement} className={styles.input} />
+          <button onClick={reset} className={styles.reset}></button>
+        </div>
       </div>
     </section>
   )
