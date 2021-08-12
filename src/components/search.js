bb82e3841597e0798939e30c4b0d5de17197a4a2
Wessel Smit <wrsmit00@gmail.com>
2021-08-12 02:32:29 +0200

    add notes

--- src/components/search.js
+++ src/components/search.js
@@ -4,6 +4,15 @@ import { fetchDetailsForPokemons, filterVariants } from '../utils/pokemons'
 import styles from '../styles/components/search.module.css'
 
 
+// TODO
+// de search werkt MAAR:
+// is enorm langzaam -> elke input change fetched enorm veel data, dit kan op 2 manieren beter:
+//  - check eerst of de pokemondata al in de state bestaat (bv. als je voor pikachu zoekt hoeft niet elke keer de data opnieuw gefetched te worden, de pikachu data staat toch al in de state)
+//  - op elke search keystroke moeten de 'vorige' fetch requests allemaal geAbort worden (met 'vorige' fetch requests bedoel ik de fetch requests die ggetriggered zijn door de vorige search keystroke want als een user snel genoeg typed dan is de computer de resultaten van je vorige keystroke nog aan het fetchen terwijl je alweer een volgend karakter hebt getyped)
+// verder is er een probleem met de useInfiniteScroll hook; deze moet gedisabled worden wanneer de gebruiker aan het zoeken is + loader.js moet niet zichtbaar zijn wanneer de gebruiker aan het zoeken is
+// ook kan de code wel wat netter (denk vooral aan de lange/dubbele namen die gebruikt worden om de pokemons op te slaan. bv: 'filteredPokemons', 'matchingPokemons', 'filteredOriginalPokemons'
+
+
 export default function Search({ pokemons, setFilteredPokemons }) {
   const [inputElement, setInputElement] = useState(null)
 
