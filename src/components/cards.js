51c10a64be3292fd102fb58cec1d748f30e85dbe
wesselsmit <wrsmit00@gmail.com>
2021-10-12 23:32:42 +0200

    feat: make card images responsive

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { displayName, displayId } from '../utils/display'
+import { classNames } from '../utils/classes'
 import styles from '../styles/components/cards.module.css'
 
 // TODO maak responsive
@@ -25,21 +26,23 @@ export default function Card({ pokemons }) {
             <li key={id} type={mainType} className={isFetched ? styles.item : null}>
               <Link href={'/' + id}>
                 <a className={styles.card}>
-                  <Image
-                    src={artwork}
-                    alt={'Image of ' + name}
-                    width={150}
-                    height={150}
-                    priority={!isFetched}
-                    className={styles.image}
-                  />
+                  <p className={ classNames(styles.id, styles.idSmall) }>{displayId(id)}</p>
+                  <div className={styles.imageContainer}>
+                    <Image
+                      src={artwork}
+                      alt={'Image of ' + name}
+                      layout={'fill'}
+                      priority={!isFetched}
+                      className={styles.image}
+                    />
+                  </div>
 
                   <article className={styles.textOuter}>
                     <div className={styles.textInner}>
                       <h6 className={styles.name}>{displayName(name)}</h6>
                       <p className={styles.japaneseName}>{japName}</p>
                     </div>
-                    <p className={styles.id}>{displayId(id)}</p>
+                    <p className={ classNames(styles.id, styles.idBig) }>{displayId(id)}</p>
                   </article>
                 </a>
               </Link>
